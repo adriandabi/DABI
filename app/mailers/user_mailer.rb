ff2075055f728e163b7c55a2f620c17e91bb73bf
adrian dabi <adrian.dabi@hotmail.com>
2019-04-10 10:23:33 -0700

    recive mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
     @message = message
+    @name = name
     mail(from: email,
        to: 'adrian.dabi@hotmail.com',
        subject: "A new contact form message from #{name}")
