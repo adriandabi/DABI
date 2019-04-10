b3993b119143357865212ce84e54e31529d99204
adrian dabi <adrian.dabi@hotmail.com>
2019-04-09 18:38:11 -0700

    added pricing to products

---
+++ db/migrate/20190410003145_price_to_products.rb
@@ -0,0 +1,5 @@
+class PriceToProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :price, :decimal, precision: 5, scale: 2, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_04_192523) do
+ActiveRecord::Schema.define(version: 2019_04_10_003145) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_192523) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.decimal "price"
   end
 
 end
