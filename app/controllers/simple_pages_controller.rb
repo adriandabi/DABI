d3bbd260750d694d102e62cd5587333d299113fa
adrian dabi <adrian.dabi@hotmail.com>
2019-04-10 01:36:24 -0700

    adding a mailer

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,13 @@ class SimplePagesController < ApplicationController
   def landing_page
     @products = Product.all
   end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'adrian.dabi@hotmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
 end
