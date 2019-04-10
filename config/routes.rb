88a22610985da9417182b89d8e5fa3e1d55ac0aa
adrian dabi <adrian.dabi@hotmail.com>
2019-04-09 23:51:34 -0700

    linking orders to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+  get 'orders/index'
   get 'products/index'
   root 'simple_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
