906eafe4f97fba204982d209e56db3d00bd606f4
adrian dabi <adrian.dabi@hotmail.com>
2019-04-10 07:20:32 -0700

    sendable mail

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -8,6 +8,9 @@ class SimplePagesController < ApplicationController
     @name = params[:name]
     @email = params[:email]
     @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+
+    # sends in developer mode
     ActionMailer::Base.mail(from: @email,
         to: 'adrian.dabi@hotmail.com',
         subject: "A new contact form message from #{@name}",
